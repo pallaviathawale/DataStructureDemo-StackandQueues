e2c19d19660a599a810eb72cf083f4fbf22e6812
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-15 22:15:29 +0530

    Add: Welcome file

---
+++ DataStructureDemoStackQueue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructureDemoStackQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure demo using Stack and Queue");
+        }
+    }
+}
